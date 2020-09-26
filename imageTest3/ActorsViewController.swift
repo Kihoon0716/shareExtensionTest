b24b4bf4d36b833e063b9a07c074e89251558f8e
Kihoon0716 <skfk3606@naver.com>
2020-09-27 00:11:48 +0900

    get main image from url
    Signed-off-by: Kihoon0716 <skfk3606@naver.com>

--- imageTest3/ActorsViewController.swift
+++ imageTest3/ActorsViewController.swift
@@ -6,27 +6,64 @@
 //
 
 import UIKit
+import SwiftLinkPreview
 
 class ActorsViewController: UIViewController {
-
-    @IBOutlet weak var actorsBtn: UILabel!
+    
+    @IBOutlet weak var imageController: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
-        actorsBtn.text = "hello"
+        let slp = SwiftLinkPreview(session: URLSession.shared, workQueue: SwiftLinkPreview.defaultWorkQueue, responseQueue: DispatchQueue.main, cache: DisabledCache.instance)
+        slp.preview("https://www.apple.com/mac/",
+                    onSuccess: {
+                        result in
+                        print("\(result.image)")
+                        print("\(result.icon)")
+                        print("\(result.title)")
+                        
+                        if let imageUrl = result.image{
+                            if let imageUrl: URL = URL(string: imageUrl){
+                                self.imageController.load(url: imageUrl)
+                            }
+                        }
+                        
+//                        self.imageController.load(url: URL(string: result.image!)!)
+                    }, onError: {error in print("error")})
     }
-
+    
+    
+    
+    
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+extension UIImageView {
+    func load(url: URL) {
+        DispatchQueue.global().async { [weak self] in
+            if let data = try? Data(contentsOf: url) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.image = image
+                    }
+                }
+            }
+        }
     }
-    */
-
 }
